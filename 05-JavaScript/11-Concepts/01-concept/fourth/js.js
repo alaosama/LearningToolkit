7b5006aaf2aa82a50c8177e2972a6f1216cfdc7a
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-02-25 22:24:22 +0100

    Update

--- 05-JavaScript/11-Concepts/01-concept/fourth/js.js
+++ 05-JavaScript/11-Concepts/01-concept/fourth/js.js
@@ -1,1 +1,12 @@
+// Write a function in JavaScript that takes a positive integer n as input and returns its factorial.
+// The factorial of a number n is the product of all integers from 1 to n.
+// const factorialOf5 = calculateFactorial(5); console.log(factorialOf5);
+// This should print: 120 (because 5! = 5 * 4 * 3 * 2 * 1 = 120)
 
+let factorielle = Number(prompt("Enter a number : "));
+
+function factorielle(){
+    if(factorielle > 0){
+            return factorielle= factorielle(factorielle-1);
+    }    
+}
