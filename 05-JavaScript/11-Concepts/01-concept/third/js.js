42cca16908dd93cbf5ad1c72295a35d970116707
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-02-25 22:21:06 +0100

    Update

--- 05-JavaScript/11-Concepts/01-concept/third/js.js
+++ 05-JavaScript/11-Concepts/01-concept/third/js.js
@@ -1,1 +1,21 @@
+// Example: Instructions: Write an algorithm in JavaScript that takes a positive integer n as input and returns the sum of all even numbers from 1 to n. 
+// If n is itself even, include it in the sum as well.
+// For n = 6, the sum of even numbers from 1 to 6 is 2 + 4 + 6 = 12. For n = 7, the sum of even numbers from 1 to 7 is 2 + 4 + 6 = 12. For n = 8, 
+// the sum of even numbers from 1 to 8 is 2 + 4 + 6 + 8 = 20.
 
+let numberenter = Number(prompt("Enter a number > 0 : "));
+
+function onput(){
+if(numberenter >= 0){
+    let numbers = numberenter;
+    for(let i=1; i=(numberenter/2)===0; i++){
+        numbers =+i;
+    }
+}
+else{
+    console.log(";;;;;;;;;;;;");
+}
+
+    return numbers;
+};
+console.log(onput);
