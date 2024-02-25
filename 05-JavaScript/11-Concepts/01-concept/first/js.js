5afab02994c792d1e0728bda927e918a12753334
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-02-25 22:15:18 +0100

    Update

--- 05-JavaScript/11-Concepts/01-concept/first/js.js
+++ 05-JavaScript/11-Concepts/01-concept/first/js.js
@@ -1,1 +1,27 @@
+// Write a program that accepts two numbers, then compares their values and prints an appropriate message in all cases.
 
+// Entrance
+// Enter first number: 5
+// Enter second number: 3
+// Expected output
+// 5 is greater than 3
+
+let first_nuber = 5;
+let second_number = 3;
+
+function solution (first_number, second_number){
+    if(first_number == second_number){
+        console.log("Equalité");
+    }
+    else if(first_number > second_number){
+        console.log(`${first_number} supérieure ${second_number}`);
+    }
+    else if(first_number < second_number){
+        console.log(`${first_number} inférieure ${second_number}`);
+    }
+    else if(first_number < 0){
+        console.log(`${first_number} est un numbre négatifs`);
+    }
+}
+
+solution(12344, 78);
